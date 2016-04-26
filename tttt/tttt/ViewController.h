7f0ed989753a516de66262eae3cb15da3b638e8b
ccLzz <870677775@qq.com>
2016-04-26 17:58:45 +0800

    d

---
+++ tttt/tttt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tttt
+//
+//  Created by 软通 on 16/4/26.
+//  Copyright © 2016年 软通. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tttt/tttt/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tttt
+//
+//  Created by 软通 on 16/4/26.
+//  Copyright © 2016年 软通. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
